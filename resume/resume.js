3964c079eff64ed50078f6be3ec1a6b51b0142eb
Kresserbash <38074110+Kresserbash@users.noreply.github.com>
2019-11-24 12:54:42 -0300

    Update resume.js

--- resume/resume.js
+++ resume/resume.js
@@ -2,7 +2,7 @@ var Algorithmia = require("algorithmia");
 
 function resume(content)
 {
-	Algorithmia.client("simkuBJvmmp9jSojH6aCTKgyN+O1")
+	Algorithmia.client("")
 	.algo("nlp/Summarizer/0.1.8")
 	.pipe(content)
 	.then(
