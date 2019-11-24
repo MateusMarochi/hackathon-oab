35c90dce693f592dd87ecc75391d4758543efbf2
Allan Ribeiro <allanribeiro14@gmail.com>
2019-11-24 12:38:05 -0300

    On branch watson 	modified:   input.js 	modified:   package-lock.json 	modified:   package.json 	new file:   watson/watson.js 	modified:   watson/watson.json

--- input.js
+++ input.js
@@ -1,5 +1,14 @@
 const readline = require('readline-sync')
-const separador = require('sbd')
+
+const watsonApiKey = require("./watson/watson.json").apikey
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
+
+var nlu = new NaturalLanguageUnderstandingV1({
+	iam_apikey: watsonApiKey,
+	version: '2019-07-12',
+	url: 'https://gateway.watsonplatform.net/natural-language-understanding/api'
+})
+
 
 function chat() {
     function recebeMensagem() {
@@ -11,9 +20,26 @@ function chat() {
     enviaMensagem(recebeMensagem())
 }
 
-function sendRequest()
+async function keyworder()
 {
-	chat.recebeMensagem()
+	return new Promise((resolve, reject) => {
+		nlu.analyze({
+			text:chat.recebeMensagem(),
+				features: {
+					keywords: {
+						"limit": 4
+					}
+				},
+				"language": "pt"
+			}, (error, response) => {
+				if(error){
+					throw error
+				}
+				console.log(JSON.stringify(response, null, 4))
+				const keywords = response.keywords.map((keyword) => {
+				return keyword.text
+			})
+			resolve(keywords)
+		})
+	})
 }
-
-chat()
---
+++ watson/watson.js
@@ -0,0 +1,4 @@
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
+
+sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+
